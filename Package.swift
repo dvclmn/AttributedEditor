608e3d839fed645034bb2c9d4266e01b0558574c
Dave Coleman <dave@dvclmn.com>
2025-11-16 09:49:28 +0100

    Removes the commented-out dependancies...
    ... and leaves just shared helpers, for now

--- Package.swift
+++ Package.swift
@@ -14,29 +14,14 @@ let package = Package(
     )
   ],
   dependencies: [
-    
-//    .package(url: "https://github.com/raspu/Highlightr.git", from: "2.2.1"),
-    //    .package(url: "https://github.com/ChimeHQ/Glyph.git", branch: "main"),
-    //    .package(url: "https://github.com/ChimeHQ/ThemePark.git", branch: "main"),
-//    .package(url: "https://github.com/dvclmn/BaseStyles.git", branch: "main"),
     .package(url: "https://github.com/dvclmn/BaseHelpers", branch: "main"),
-//    .package(url: "https://github.com/ChimeHQ/Rearrange.git", from: "2.0.0"),
-//    .package(url: "https://github.com/ChimeHQ/Neon", branch: "main"),
-//    .package(url: "https://github.com/alex-pinkus/tree-sitter-swift", branch: "with-generated-files"),
-//    .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-markdown.git", branch: "split_parser"),
-//    .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro.git", from: "0.5.0"),
+
   ],
   targets: [
     .target(
       name: "MarkdownEditor",
       dependencies: [
-//        "Neon",
-//        "Rearrange",
-//        .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro"),
-//        .product(name: "TreeSitterSwift", package: "tree-sitter-swift"),
-//        .product(name: "TreeSitterMarkdown", package: "tree-sitter-markdown"),
         .product(name: "SharedHelpers", package: "BaseHelpers"),
-//        .product(name: "BaseStyles", package: "BaseStyles"),
       ]
     ),
   ]
