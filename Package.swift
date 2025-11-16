80faed39dcb23fd57ddc6bc023bff6678d2590df
Dave Coleman <dave@dvclmn.com>
2025-11-16 09:48:52 +0100

    Early work to clean things up, and introduce...
    ... a newer take on the text view, hopefully with more robust support for any source code, not just markdown

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version: 6.2
 
 import PackageDescription
 
@@ -18,41 +18,26 @@ let package = Package(
 //    .package(url: "https://github.com/raspu/Highlightr.git", from: "2.2.1"),
     //    .package(url: "https://github.com/ChimeHQ/Glyph.git", branch: "main"),
     //    .package(url: "https://github.com/ChimeHQ/ThemePark.git", branch: "main"),
-    .package(url: "https://github.com/dvclmn/BaseStyles.git", branch: "main"),
-    .package(url: "https://github.com/dvclmn/BaseHelpers.git", branch: "main"),
-    .package(url: "https://github.com/ChimeHQ/Rearrange.git", from: "2.0.0"),
-    .package(url: "https://github.com/ChimeHQ/Neon", branch: "main"),
-    .package(url: "https://github.com/alex-pinkus/tree-sitter-swift", branch: "with-generated-files"),
-    .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-markdown.git", branch: "split_parser"),
-    .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro.git", from: "0.5.0"),
+//    .package(url: "https://github.com/dvclmn/BaseStyles.git", branch: "main"),
+    .package(url: "https://github.com/dvclmn/BaseHelpers", branch: "main"),
+//    .package(url: "https://github.com/ChimeHQ/Rearrange.git", from: "2.0.0"),
+//    .package(url: "https://github.com/ChimeHQ/Neon", branch: "main"),
+//    .package(url: "https://github.com/alex-pinkus/tree-sitter-swift", branch: "with-generated-files"),
+//    .package(url: "https://github.com/tree-sitter-grammars/tree-sitter-markdown.git", branch: "split_parser"),
+//    .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro.git", from: "0.5.0"),
   ],
   targets: [
     .target(
       name: "MarkdownEditor",
       dependencies: [
-        "Neon",
-        "Rearrange",
-        .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro"),
-        .product(name: "TreeSitterSwift", package: "tree-sitter-swift"),
-        .product(name: "TreeSitterMarkdown", package: "tree-sitter-markdown"),
-        .product(name: "BaseHelpers", package: "BaseHelpers"),
-        .product(name: "BaseStyles", package: "BaseStyles"),
+//        "Neon",
+//        "Rearrange",
+//        .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro"),
+//        .product(name: "TreeSitterSwift", package: "tree-sitter-swift"),
+//        .product(name: "TreeSitterMarkdown", package: "tree-sitter-markdown"),
+        .product(name: "SharedHelpers", package: "BaseHelpers"),
+//        .product(name: "BaseStyles", package: "BaseStyles"),
       ]
     ),
   ]
 )
-
-
-let swiftSettings: [SwiftSetting] = [
-  .enableExperimentalFeature("StrictConcurrency"),
-  .enableUpcomingFeature("DisableOutwardActorInference"),
-  .enableUpcomingFeature("InferSendableFromCaptures"),
-  .enableUpcomingFeature("BareSlashRegexLiterals"),
-]
-
-for target in package.targets {
-  var settings = target.swiftSettings ?? []
-  settings.append(contentsOf: swiftSettings)
-  target.swiftSettings = settings
-}
-
