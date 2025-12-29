733519d4974f998cdbd8ca42523d866fc744b826
Dave Coleman <dave@dvclmn.com>
2025-12-29 12:49:26 +0100

    Removes neon dep. for now

--- Package.swift
+++ Package.swift
@@ -20,13 +20,13 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/dvclmn/BaseHelpers", branch: "main"),
-        .package(url: "https://github.com/mattmassicotte/nsui", from: "1.3.0"),
+    .package(url: "https://github.com/mattmassicotte/nsui", from: "1.3.0"),
     //    .package(url: "https://github.com/pointfreeco/swift-sharing", from: "2.7.4"),
     //    .package(url: "https://github.com/ChimeHQ/Rearrange", from: "2.0.0"),
     //    .package(url: "https://github.com/ChimeHQ/Glyph", branch: "main"),
-    .package(url: "https://github.com/ChimeHQ/ThemePark", branch: "main"),
-    .package(url: "https://github.com/ChimeHQ/Lowlight", branch: "main"),
-    .package(url: "https://github.com/ChimeHQ/Neon", from: "0.6.0"),
+    //    .package(url: "https://github.com/ChimeHQ/ThemePark", branch: "main"),
+    //    .package(url: "https://github.com/ChimeHQ/Lowlight", branch: "main"),
+    //    .package(url: "https://github.com/ChimeHQ/Neon", from: "0.6.0"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
 
   ],
@@ -36,7 +36,6 @@ let package = Package(
       dependencies: [
         .local(.editorCore),
         .local(.markdown),
-        .exteral(.themePark),
         .exteral(.coreTools),
       ]
     ),
@@ -47,9 +46,6 @@ let package = Package(
         .local(.markdown),
         .exteral(.colourKit),
         .exteral(.coreTools),
-        .exteral(.neon),
-        .exteral(.themePark),
-        .exteral(.lowlight),
         .exteral(.nsui),
       ]
     ),
@@ -59,9 +55,7 @@ let package = Package(
       dependencies: [
         .exteral(.colourKit),
         .exteral(.coreTools),
-        .exteral(.neon),
-        .exteral(.themePark),
-        .exteral(.lowlight),
+
       ]
     ),
 
@@ -105,9 +99,9 @@ enum PackageModule {
 enum ExternalDependency {
   case colourKit
   case coreTools
-  case themePark
-  case neon
-  case lowlight
+  //  case themePark
+  //  case neon
+  //  case lowlight
   case nsui
 
   /// If different to product name
@@ -115,15 +109,15 @@ enum ExternalDependency {
     switch self {
       case .colourKit: "ColourKit"
       case .coreTools: "CoreTools"
-      case .themePark, .neon, .lowlight, .nsui: nil
+      case .nsui: nil
     }
   }
   var package: String {
     switch self {
       case .colourKit, .coreTools: "BaseHelpers"
-      case .themePark: "ThemePark"
-      case .neon: "Neon"
-      case .lowlight: "Lowlight"
+      //      case .themePark: "ThemePark"
+      //      case .neon: "Neon"
+      //      case .lowlight: "Lowlight"
       case .nsui: "NSUI"
     }
   }
